6bc6a2fb0484e12f1143ecef5340e344ca535b58
Sergey Chechaev <2netme@gmail.com>
2016-10-21 13:29:45 +0300

    meta api release

--- lib/buyon_api/client.rb
+++ lib/buyon_api/client.rb
@@ -5,9 +5,18 @@ module BuyonApi
       @conn = BuyonApi::Api.call
     end
 
-    def method_missing(m, *args, &block)
-      require 'pry'; binding.pry
-      puts "There's no method called #{m} here -- please try again."
+    def method_missing(m, args, &block)
+      result = @conn.post do |req|
+        req.url m.to_s
+        req.headers['Content-Type'] = 'application/json'
+        req.params = args
+      end
+
+      obj_hash JSON.parse(result.body)
+    end
+
+    def obj_hash(result)
+      OpenStruct.new(result)
     end
 
   end
--- spec/buyon_api/client_spec.rb
+++ spec/buyon_api/client_spec.rb
@@ -4,7 +4,9 @@ describe BuyonApi::Client do
   context "login" do
     it "return token" do
       conn = BuyonApi::Client.new
-      require 'pry'; binding.pry
+      # you can uncomment bindeing.pry and try
+      # result = conn.login(password: "password", login: "test@mail.ru")
+      # require 'pry'; binding.pry
     end
   end
 end
